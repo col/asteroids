7775758bceca51de6c451aaf95f05383b4c2516a
Colin Harris <colin.harris@lambdasoftware.com.au>
2012-02-21 10:15:43 +1100

    Added transparency and slightly better collision detection for asteroids (as circles)

--- asteroid.rb
+++ asteroid.rb
@@ -5,17 +5,30 @@ include Rubygame
 class Asteroid < GameObject
 
   def initialize(position, size, angle, speed, game)
-    super(position, [100,100])
-
-    @game = game
+    super(position, size, game)
 
     radians = -(angle+90) * Math::PI / 180
     @velocity = Ftor.new_am radians, speed
 
-    # draw the asteroid (a circle will do for now)
-    @colour = [255, 255, 255]
+    # Draw the asteroid (a circle will do for now)
     center = size.map { |x| x/2 }
     @image.draw_circle center, center[0]-0.1, @colour
+    @image.set_alpha 0,0
+  end
+
+  # Detect a collision with a circle by checking if the distance
+  # between the point and the circles center is smaller than it's radius.
+  def collide_point?(point)
+    distance = Math.hypot(point[0]-@rect.center[0],point[1]-@rect.center[1])
+    distance < @rect.w/2
+  end
+
+  # Override to detect collision between circle and rect.
+  def collide_rect?(rect)
+    return true if collide_point?(rect.topleft)
+    return true if collide_point?(rect.topright)
+    return true if collide_point?(rect.bottomleft)
+    collide_point?(rect.bottomright)
   end
 
 end
@@ -27,8 +40,17 @@ class BigAsteroid < Asteroid
   end
 
   def hit
-    @game.asteroids << SmallAsteroid.new(@rect.center, @velocity.angle+15, @velocity.magnitude*1.2, @game)
-    @game.asteroids << SmallAsteroid.new(@rect.center, @velocity.angle-15, @velocity.magnitude*1.2, @game)
+    # Add two small asteroids based on the parent asteroid
+    (1..2).each do
+      # randomise the position of the new asteroid but keep it within its parent
+      position = @rect.topleft.map { |i| i + rand(@rect.w) }
+      # randomise the angle but keep it within +/- 90 degrees of the parent angle
+      degrees = -(@velocity.angle * 180 / Math::PI)-90
+      degrees += rand(180) - 90
+      # increase the speed by a random amount (between 100% - 20%)
+      speed = @velocity.magnitude * (1 + rand())
+      @game.add_asteroid SmallAsteroid.new(position, degrees, speed, @game)
+    end
     self.kill
   end
 
